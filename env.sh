32f67db0bcac765a426cb4f2ad1332f88f331e1a
breizh5729 <vincn.ledan@gmail.com>
2020-02-13 21:34:14 +0100

    a dd cloud formation

--- env.sh
+++ env.sh
@@ -1,8 +1,12 @@
 #!/bin/bash 
  export node1=$(nmap 10.10.4.*  | grep report | awk '{print $NF}' |sed -n '2,2p') 
+
  export node2=$(nmap 10.10.4.*  | grep report | awk '{print $NF}' |sed -n '3,3p') 
 
 sudo echo $node1 node1 >> /etc/hosts 
 sudo echo $node2 node2 >> /etc/hosts 
 echo " votre environement est prêt "
+
+git clone https://github.com/breizh5729/formation_ansible.git
+cd formation_ansible && ls -ltr
 
