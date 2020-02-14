d4b9f9786aba780c2994f5caaa63627a2c80e42f
breizh5729 <vincn.ledan@gmail.com>
2020-02-14 11:41:33 +0100

    env.sh

--- env.sh
+++ env.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-echo " installation des packets "  
-yum install -y nmap 
-pip install ansible 
+echo " installation des packets "
+yum install -y nmap
+pip install ansible
 
 echo " mise à jour du fichier /etc/hosts "
-range = $(curl http://169.254.169.254/latest/meta-data/hostname | rev | cut -d"." -f3 | rev)
- export node1=$(nmap 10.10.$range.*  | grep report | awk '{print $NF}' |sed -n '2,2p') 
+export range=$(curl http://169.254.169.254/latest/meta-data/hostname | rev | cut -d"-" -f2 | rev)
+export node1=$(nmap 10.10.$range.*  | grep report | awk '{print $NF}' |sed -n '2,2p')
+export node2=$(nmap 10.10.$range.*  | grep report | awk '{print $NF}' |sed -n '3,3p')
 
- export node2=$(nmap 10.10.$range.*  | grep report | awk '{print $NF}' |sed -n '3,3p') 
+sudo echo $node1 node1 >> /etc/hosts
+sudo echo $node2 node2 >> /etc/hosts
 
-sudo echo $node1 node1 >> /etc/hosts 
-sudo echo $node2 node2 >> /etc/hosts 
-
-echo " réccupération clès ssh " 
+echo " réccupération clès ssh "
 aws s3 cp s3://formationansiblecontinuous/aws1.pem ../.ssh/id_rsa
-chmod 600 ../.ssh/id_rsa 
-
-echo " vérification de la connexion ssh en cours " 
-ssh ec2-user@node1 " hostname " 
-ssh ec2-user@node2 " hostname " 
+chmod 600 ../.ssh/id_rsa
+chown ec2-user:ec2-user ../.ssh/id_rsa
+echo " vérification de la connexion ssh en cours "
+ssh ec2-user@node1 " hostname   "
+ssh ec2-user@node2 " hostname "
 
 echo " votre environement est prêt "
-
+echo " tenter d'aller sur les serveurs node1 et node2 "
